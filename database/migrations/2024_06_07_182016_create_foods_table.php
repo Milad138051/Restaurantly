c52ba415ad4921589cc8ecad0b4411b45c0cd643
Milad <mh3yny@gamil.com>
2024-06-07 22:26:22 +0330

    create migrations

---
+++ database/migrations/2024_06_07_182016_create_foods_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->text('image');
+            $table->decimal('price', 20, 3);
+            $table->tinyInteger('status')->default(0);
+            $table->foreignId('category_id')->constrained('categories')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('foods');
+    }
+};
